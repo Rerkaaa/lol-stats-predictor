2268a4a097a7c658751613d4bc6812e83afb82ec
Rerkaaa <your-github-email@example.com>
2026-07-25 03:12:43 +0200

    Offset KDA analysis heading

--- public/full-match-view.js
+++ public/full-match-view.js
@@ -71,5 +71,6 @@ document.head.insertAdjacentHTML('beforeend', `<style id="scorecard-heading-posi
 .scorecard-match-panel .analysis-team header>strong{position:absolute;left:12px;top:10px}
 .scorecard-match-panel .analysis-team header>span{position:absolute;left:64px;top:10px;margin:0!important}
 .scorecard-match-panel .analysis-columns{position:absolute!important;left:31.5%!important;top:0!important;width:68.5%!important;height:36px;align-items:center;gap:14px!important;grid-template-columns:50px 78px 94px 65px 54px minmax(0,1fr)!important}
+.scorecard-match-panel .analysis-columns span:nth-child(2){transform:translateX(5px)}
 @media(max-width:850px){.scorecard-match-panel .analysis-columns{left:31.5%!important;width:68.5%!important;grid-template-columns:44px 67px 80px 57px 48px minmax(0,1fr)!important}}
 </style>`);
