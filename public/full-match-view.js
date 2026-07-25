91e997d42cbe7354b820b9bfda8f3d1577dc0f17
Rerkaaa <your-github-email@example.com>
2026-07-25 03:15:15 +0200

    Align match analysis column labels to values

--- public/full-match-view.js
+++ public/full-match-view.js
@@ -71,6 +71,9 @@ document.head.insertAdjacentHTML('beforeend', `<style id="scorecard-heading-posi
 .scorecard-match-panel .analysis-team header>strong{position:absolute;left:12px;top:10px}
 .scorecard-match-panel .analysis-team header>span{position:absolute;left:64px;top:10px;margin:0!important}
 .scorecard-match-panel .analysis-columns{position:absolute!important;left:31.5%!important;top:0!important;width:68.5%!important;height:36px;align-items:center;gap:14px!important;grid-template-columns:50px 78px 94px 65px 54px minmax(0,1fr)!important}
-.scorecard-match-panel .analysis-columns span:nth-child(2){transform:translateX(5px)}
+.scorecard-match-panel .analysis-columns span:nth-child(2){transform:translateX(13px)}
+.scorecard-match-panel .analysis-columns span:nth-child(3){transform:translateX(25px)}
+.scorecard-match-panel .analysis-columns span:nth-child(4){transform:translateX(45px)}
+.scorecard-match-panel .analysis-columns span:nth-child(5),.scorecard-match-panel .analysis-columns span:nth-child(6){transform:translateX(52px)}
 @media(max-width:850px){.scorecard-match-panel .analysis-columns{left:31.5%!important;width:68.5%!important;grid-template-columns:44px 67px 80px 57px 48px minmax(0,1fr)!important}}
 </style>`);
